69ba18b3a876785214591206afa818621c94147f
anilphub <soft.akp@gmail.com>
2022-05-18 22:21:39 +0530

    action test 3

--- containerdemo/src/main/java/com/docker/ex/containerdemo/ContainerdemoApplication.java
+++ containerdemo/src/main/java/com/docker/ex/containerdemo/ContainerdemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-//Demo Apps class
+//Demo Apps
 @SpringBootApplication
 public class ContainerdemoApplication {
 
